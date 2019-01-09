3dbdc3a6ac4b0af4945b5fb617a376cadd93b52e
Markus Flodmark <markus.flodmark@gmail.com>
2019-01-09 21:09:28 +0100

    startpage default

---
+++ EpiserverPROG17/Models/Pages/StartPage.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using EPiServer.Core;
+using EPiServer.DataAbstraction;
+using EPiServer.DataAnnotations;
+using EPiServer.SpecializedProperties;
+
+namespace EpiserverPROG17.Models.Pages
+{
+    [ContentType(DisplayName = "Start", 
+        GUID = "088b0f68-e005-44d1-9cce-127af7c40c97", 
+        Description = "Used as start page")]
+    public class StartPage : PageData
+    {
+        /*
+                [CultureSpecific]
+                [Display(
+                    Name = "Main body",
+                    Description = "The main body will be shown in the main content area of the page, using the XHTML-editor you can insert for example text, images and tables.",
+                    GroupName = SystemTabNames.Content,
+                    Order = 1)]
+                public virtual XhtmlString MainBody { get; set; }
+         */
+    }
+}
